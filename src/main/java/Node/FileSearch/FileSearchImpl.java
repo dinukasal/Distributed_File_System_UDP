1688db6218f9c0f3ff9f7ea086c49f6963b893c8
dinuka <dinukasal@gmail.com>
2017-11-25 06:51:53 +0000

    update

--- src/main/java/Node/FileSearch/FileSearchImpl.java
+++ src/main/java/Node/FileSearch/FileSearchImpl.java
@@ -100,8 +100,8 @@ public class FileSearchImpl implements FileSearch {
             System.out.println("File Found in....");
             System.out.println("Owner IP : " + ownerIP);
             System.out.println("Owner Port : " + ownerPort);
-            System.out.println("Number of Hops : " + (3 - numberOfHops));
-            hops=numberOfHops;
+            System.out.println("Number of Hops : " + (numberOfHops-MAX_HOPS));
+            hops=numberOfHops-MAX_HOPS;
 
             ArrayList<String[]> searchResultsToDisplay = new ArrayList<String[]>();
             fileFoundState=1;            
